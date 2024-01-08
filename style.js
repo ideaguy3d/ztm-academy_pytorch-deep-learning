077930447d28b3fc5565af1ad9bd7c01e42b0840
Julius Hernandez <julius_hernandez@apple.com>
2024-01-07 23:05:51 -0700

    Create style.js

---
+++ style.js
@@ -0,0 +1,21 @@
+/*
+.md-main__inner {
+    display: flex;
+    height: 100%;
+    margin-top: 1.5rem;
+    width: 80%;
+}
+
+.md-grid {
+    margin-left: auto;
+    margin-right: auto;
+    max-width: none;
+}
+
+*/
+
+document.querySelector('.md-main__inner').style.cssText = "display: flex; height: 100%; margin-top: 1.5rem; width: 80%;";
+document.querySelector('.md-grid').style.cssText = "margin-left: auto; margin-right: auto; max-width: none;";
+
+
+//
